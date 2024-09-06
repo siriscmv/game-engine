9d7e910dc7f29b81037b735cb69846bee6710d81
Cyril <cvincen@ncsu.edu>
2024-09-05 22:05:28 -0400

    chore: setup engine for submission

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -3,54 +3,5 @@
 
 // Use this file to experiment with the Engine
 int main(int argc, char** argv) {
-	GameEngine gameEngine("CSC 581 Game Engine");
-
-	Entity rect(Position(500, 500), Size(100, 100));
-	Entity rect2(Position(650, 500), Size(100, 100));
-	Entity platform(Position(0, 800), Size(1080, 20));
-
-	platform.setEntityType(EntityType::FIXED);
-
-	// Create an array of entities
-	std::vector<Entity*> entities;
-	entities.push_back(&rect);
-	entities.push_back(&rect2);
-	entities.push_back(&platform);
-
-	// Initializing the engine
-	if (!gameEngine.initialize(entities)) {
-		return -1;
-	}
-
-	gameEngine.getPhysicsSystem()->applyPhysics(rect, 9);
-	gameEngine.getPhysicsSystem()->applyPhysics(rect2, 9);
-
-	// Define bindings for key presses
-	const std::function<void()> exitCallback = [&gameEngine]() {
-		gameEngine.setGameState(GameState::EXIT);
-	};
-
-	// Key bind for screen scaling mode flip
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_S, "toggle_scaling", [&gameEngine]() {
-		gameEngine.toggleScalingMode();
-		});
-
-	
-	// Key binds for entity movement
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_LEFT, "move_left", [&rect]() { rect.setVelocityX(-50.f); });
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_RIGHT, "move_right", [&rect]() { rect.setVelocityX(50.0f); });
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_UP, "move_up", [&rect]() { rect.setVelocityY(-50.0f); });
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_DOWN, "move_down", [&rect]() { rect.setVelocityY(50.0f); });
-	
-	// Key binds for exiting the game engine
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_ESCAPE, "esc_exit", exitCallback);
-	gameEngine.getInputManager()->bind(SDL_SCANCODE_Q, "q_exit", exitCallback);
-
-	// Running the game loop
-	gameEngine.run();
-
-	// Shutting down the engine
-	gameEngine.shutdown();
-
-	return 0;
+	printf("The Game Engine has compiled successfully");
 }
